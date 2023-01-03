c3ecd18d28e775dd3eab6d9937908690289db80e
Kassandra Johnson <kassandra.m.johnson@gmail.com>
2023-01-03 12:31:46 -0500

    Completed Dapper Exercise 1

---
+++ DapperExercise/DapperDeptRepo.cs
@@ -0,0 +1,28 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DapperExercise
+{
+    internal class DapperDeptRepo : IDeptRepo
+    {
+        private readonly IDbConnection _connection;
+        public DapperDeptRepo(IDbConnection connection)
+        {
+            _connection = connection;
+        }
+        public IEnumerable<Department> GetAllDepartments()
+        {
+            return _connection.Query<Department>("SELECT * FROM Departments;");
+        }
+        public void InsertDepartment(string newDeptName)
+        {
+            _connection.Execute("INSERT INTO DEPARTMENTS (Name) VALUES (@departmentName);",
+             new { departmentName = newDeptName });
+        }
+    }
+}
--- DapperExercise/Program.cs
+++ DapperExercise/Program.cs
@@ -1,10 +1,37 @@
-﻿namespace DapperExercise
+﻿using System;
+using System.Data;
+using System.IO;
+using MySql.Data.MySqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace DapperExercise
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+            string connString = config.GetConnectionString("DefaultConnection");
+            IDbConnection conn = new MySqlConnection(connString);
+            DapperDeptRepo repo = new DapperDeptRepo(conn);
+            IEnumerable<Department> departments = repo.GetAllDepartments();
+            Console.WriteLine("Here is a list of current departments at Best Buy:");
+            foreach (var dept in departments)
+            {
+                Console.WriteLine($"Dept ID: {dept.DepartmentID}, Dept Name: {dept.Name}");
+            }
+            Console.WriteLine("Please create a new department for Best Buy by entering the name below:");
+            string userDept = Console.ReadLine();
+            repo.InsertDepartment(userDept);
+            IEnumerable<Department> updatedDepts = repo.GetAllDepartments();
+            Console.WriteLine("Great idea!  Here is the updated list of departments:");
+            foreach (var dept in updatedDepts)
+            {
+                Console.WriteLine($"Dept ID: {dept.DepartmentID}, Dept Name: {dept.Name}");
+            }
         }
     }
 }
